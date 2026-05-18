46bfdfd79880cbf3e3642d857cbaa3ec37dadd2c
Никита Ягодкин <yagodkin_na@drom.ru>
2026-05-18 22:54:27 +0700

    Переименовать мод в ertOres

--- src/client/java/com/ertores/client/ErtoresModClient.java
+++ src/client/java/com/ertores/client/ErtoresModClient.java
@@ -1,8 +1,8 @@
-package com.example.client;
+package com.ertores.client;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class ErtoresModClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
